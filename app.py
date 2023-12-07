d3fb0ab8b2af8e1cb1c41ea9f5d606807908355a
bshashank-81 <shashank.balabhadra@zemosolabs.com>
2023-12-07 11:29:07 +0530

    Add buggycode

--- app.py
+++ app.py
@@ -75,6 +75,25 @@ print("Result:", result)
 text = None
 print(len(text))
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    user_input = request.args.get('query')
+    
+    # Intentional vulnerability: Concatenating user input directly into SQL query
+    query = f"SELECT * FROM users WHERE username = '{user_input}'"
+    
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    
+    return render_template('search_results.html', results=result)
+
+
 # Run the application if executed directly
 if __name__ == '__main__':
     app.run(debug=True)
